fb92fb88ad3874ecf3abc27cf926dbbc625507b1
madhanrajan <madhanrajan.varadharajan@gmail.com>
2023-11-17 22:49:34 +0800

    Initial Commit

---
+++ SwiftUIWithMetal/SwiftUIWithMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithMetalApp.swift
+//  SwiftUIWithMetal
+//
+//  Created by Madhan on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
